32662af36845ed505627ec45724e5b4e343355e2
thenmal <thenmal@9f315c02-1ec2-11df-9b44-d1afe4a40082>
2010-03-15 01:11:44 +0000

    blar blar

--- trunk/src/client/Model.py
+++ trunk/src/client/Model.py
@@ -5,6 +5,14 @@ Created on Mar 9, 2010
 '''
 
 class Blar:
+
+    # facing is based on cardinal direction
+    # 1 2 3
+    # 4 * 5
+    # 6 7 8
+    facing = 7
+    # walkFrame is for animation
+    walkFrame = 0
     
     def __init__(self, x, y):
         self.x = x
@@ -36,3 +44,27 @@ class EncapsTest:
     def has(self, index):
         return index in self.set.keys()
         
+class Packet:
+    packet = []
+    move = 0
+    add = 1
+    remove = 2
+
+    def __init__(self, id):
+        self.id = id
+
+    def getPacket(self):
+        return self.id, self.packet
+
+    #move packet will be id 0
+    #obj must be a blar
+    #packet will be (id, (x, y))
+    def addMove(self, id, obj):
+        loc = obj.getPos()
+        self.packet[len(self.packet)] = self.move, (id, loc)
+
+    def addObj(self, id, obj):
+        self.packet[len(self.packet)] = self.add, (id, obj)
+
+    def removeObj(self, id):
+        self.packet[len(self.packet)] = self.remove, id

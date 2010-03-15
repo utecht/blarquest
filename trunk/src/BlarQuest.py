228a6afdeb96a9ece26833615d40acfd85494fe6
thenmal <thenmal@9f315c02-1ec2-11df-9b44-d1afe4a40082>
2010-03-15 01:49:39 +0000

    blar blar

--- trunk/src/BlarQuest.py
+++ trunk/src/BlarQuest.py
@@ -3,10 +3,34 @@ Created on Feb 21, 2010
 
 @author: Lamneth
 '''
+import sys, os
+sys.path.append( os.path.join( os.getcwd(), './client') )
+from Screen import *
+from Model import *
+from ClientTest import *
+import time
+
+
+def draw(screen):
+    print('Drawing')
+    screen.draw
+
+def think(model):
+    print('Thinking...')
 
 def main():
     # the main code goes here
-    print('Hello World')
+    print('Blar Quest')
+    # main game will have 2 threads, the neworking and screen thread
+    model = Model()
+    client = Client(model)
+    screen = Screen(model)
+    running = True
+    client.start()
+    while(running):
+        think(model, client)
+        draw(screen)
+        time.sleep(.1)
  
 if __name__ == "__main__":
     main()
